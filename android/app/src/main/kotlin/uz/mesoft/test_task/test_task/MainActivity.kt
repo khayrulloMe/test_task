ff8b1fdff0c18a89fc0818d2b33a092972e9f1e7
khayrullo <khayrullo.me@gmail.com>
2023-04-19 14:12:47 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/uz/mesoft/test_task/test_task/MainActivity.kt
@@ -0,0 +1,13 @@
+package uz.mesoft.test_task.test_task
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.ViewGroup
+import android.widget.Button
+import io.flutter.embedding.android.FlutterActivity
+
+
+class MainActivity: FlutterActivity() {
+
+
+}
